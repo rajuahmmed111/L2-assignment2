140766a437edffc1e7d5dc22be5bd982cac6bbb6
rajuahmmed111 <ahraju2003@gmail.com>
2024-05-23 17:35:02 +0600

    5th added product.route.ts product.controller.ts and product.service.ts

---
+++ src/Modules/product.controller.ts
@@ -0,0 +1,105 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express';
+import { ProductServices } from './product.service';
+import { productZodSchema } from './product.zod.validation';
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+
+    // create zod validation
+    const zodValidation = productZodSchema.parse(productData);
+
+    const result = await ProductServices.createProduct(zodValidation);
+    res.status(200).json({
+      success: true,
+      message: 'Product is create successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: err,
+    });
+  }
+};
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const searchTerm = req.query.searchTerm;
+    const result = await ProductServices.getAllProducts(searchTerm);
+    res.status(200).json({
+      success: true,
+      message: 'Products are fetched successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: err,
+    });
+  }
+};
+const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+    const result = await ProductServices.getProductById(productId);
+    res.status(200).json({
+      success: true,
+      message: 'Product is fetched successfully',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: err,
+    });
+  }
+};
+
+const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.productId;
+    // const updateData = req.body;
+    const result = await ProductServices.updateProduct(productId);
+    res.status(200).json({
+      success: true,
+      message: 'Product updated successfully!',
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: err,
+    });
+  }
+};
+
+const deletedProductById = async (req: Request, res: Response) => {
+  try {
+    const { deletedProductId } = req.params;
+    await ProductServices.deletedProductById(deletedProductId);
+    res.status(200).json({
+      success: true,
+      message: 'Product deleted successfully',
+      data: null,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message || 'Something went wrong',
+      error: err,
+    });
+  }
+};
+
+export const ProductController = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deletedProductById,
+};
---
+++ src/Modules/product.service.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TProduct } from './product.interface';
+import Product from './project.model';
+
+const createProduct = async (product: TProduct) => {
+  const result = await Product.create(product);
+  return result;
+};
+
+const getAllProducts = async (searchTerm: any) => {
+  const regex = new RegExp(searchTerm, 'i');
+  const result = await Product.find({ description: { $regex: regex } });
+  return result;
+};
+
+const getProductById = async (productId: string) => {
+  const result = await Product.findById({ _id: productId });
+  return result;
+};
+
+const updateProduct = async (productId: string) => {
+  const result = await Product.findOneAndUpdate(
+    { _id: productId },
+    {
+      $set: {
+        name: 'Bluetooth Speaker',
+      },
+    },
+    { new: true },
+  );
+
+  return result;
+};
+
+const deletedProductById = async (deletedProductId: string) => {
+  const result = await Product.deleteOne(
+    { deletedProductId },
+    { isDeleted: true },
+  );
+  return result;
+};
+
+export const ProductServices = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deletedProductById,
+};
